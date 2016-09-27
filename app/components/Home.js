c60621fe4f16ba33d243d1e92a456fadb9d10091
harebu <bugu1986@126.com>
2016-09-27 14:45:38 +0800

    add project

--- app/components/Home.js
+++ app/components/Home.js
@@ -1,17 +1,80 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router';
-import styles from './Home.css';
+//import styles from './Home.css';
+
+import { Button,Icon, Row, Col,Modal,Card ,Input } from 'antd';
+const {dialog} = require('electron').remote;
 
 
 export default class Home extends Component {
+
+  constructor() {
+    super();
+    this.state = {
+      loading: false,
+      visible: false,
+      filePath:''
+    };
+    this.handleOk = this.handleOk.bind(this);
+    this.showModal = this.showModal.bind(this);
+    this.handleCancel = this.handleCancel.bind(this);
+    this.fileLocation = this.fileLocation.bind(this);
+  }
+
+  showModal() {
+    this.setState({
+      visible: true,
+    });
+  }
+
+  handleOk() {
+    this.setState({loading: true});
+    setTimeout(() => {
+      this.setState({loading: false, visible: false});
+    }, 3000);
+  }
+
+  handleCancel() {
+    this.setState({visible: false});
+  }
+
+  fileLocation(){
+    let filePath = dialog.showOpenDialog({properties: ['openDirectory']});
+    this.setState({filePath:filePath});
+  }
+
   render() {
+
+    const button = <Button type="primary" icon="ellipsis" onClick={this.fileLocation}></Button>;
+
     return (
-      <div>
-        <div className={styles.container}>
-          <h2>Home</h2>
-          <Link to="/counter">to Counter</Link>
+        <div>
+          <div style={{alignItems:'center',padding:20}}>
+            <Button type="primary" shape="circle"><Icon type="user"/></Button>
+            <text style={{marginLeft:20,fontSize:20}}>buhe</text>
+          </div>
+          <div>
+            <Button style={{margin:10}} onClick={this.showModal}><Icon type="plus-circle-o"/>New Project</Button>
+            <Button style={{margin:10}}><Icon type="folder-open"/>Open Project</Button>
+          </div>
+
+          <Modal ref="modal"
+                 visible={this.state.visible}
+                 title="New Project" onOk={this.handleOk} onCancel={this.handleCancel}
+                 footer={[
+                            <Button key="back" type="ghost" size="large" onClick={this.handleCancel}>Cancel</Button>,
+                            <Button key="submit" type="primary" size="large" loading={this.state.loading} onClick={this.handleOk}>
+                              New Project
+                            </Button>,
+                        ]}
+              >
+            <div>
+              <Input style={{margin:10}} placeholder="Project Name" />
+              <Input style={{margin:10}} placeholder="Project Location" addonAfter={button} value={this.state.filePath}/>
+            </div>
+
+          </Modal>
         </div>
-      </div>
     );
   }
 }
---
+++ app/sdk/Project.js
@@ -0,0 +1,10 @@
+/**
+ * Created by buhe on 16/9/27.
+ */
+
+export default class Project {
+
+  newDebugProject(name,location){
+
+  }
+}
